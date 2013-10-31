2ff4eda86bcfc7a2fb7483dc745d686f2bf617a1
GroundskeeperWillie <portlyowl@gmail.com>
2013-10-31 16:19:25 +1100

    Initial commit & static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Forgefire::Application.config.secret_key_base = '5bdf7455a0d8fa56ea6e450993ee094e1607dd606b2a105d21c9a68400c2e795e2e1af8fa76cac92e0b239b7344b014c492293a63392d479d452132b3640f50f'
